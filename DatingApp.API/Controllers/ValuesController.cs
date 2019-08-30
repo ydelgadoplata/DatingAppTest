6491c0b65e1c367651b1a7d61afbc4c2dfdf3dbe
Yesid Delgado Plata <ydelgadoplata@gmail.com>
2019-08-30 17:48:56 -0500

    Register&Login added & stored in DB

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         private readonly DataContext _context;
+
+        public DataContext Context => _context;
+
         public ValuesController(DataContext context)
         {
             _context = context;
@@ -23,17 +28,18 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
-            var values = await _context.Values.ToListAsync();
+            var values = await Context.Values.ToListAsync();
 
             return Ok(values);
             // return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            var value = await Context.Values.FirstOrDefaultAsync(x => x.Id == id);
 
             return Ok(value);
         }
