a4c6471133d227aa98478ec493c8ba036710cfd1
ydelgadoplata <ydelgadoplata@gmail.com>
2019-10-24 15:52:58 -0500

    Componentes de terceros: Ngx Bootstrap JWT library Bootswatch

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,26 +12,17 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 
-
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      BrowserAnimationsModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider
-   ],
-   bootstrap: [
-      AppComponent,
-   ]
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [AuthService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- DatingAppSPA/src/app/register/register.component.ts
+++ DatingAppSPA/src/app/register/register.component.ts
@@ -1,31 +1,37 @@
-  import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-  import { AuthService } from '../_services/auth.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
-  @Component({
-    selector: 'app-register',
-    templateUrl: './register.component.html',
-    styleUrls: ['./register.component.css']
-  })
-  export class RegisterComponent implements OnInit {
-    @Input() valuesFromHome: any;
-    @Output() cancelRegister = new EventEmitter();
-    model: any = {};
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
 
-    constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-    ngOnInit() {
-    }
+  ngOnInit() {}
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      console.log('Registration OK!');
-    }, error => {
-      console.log(error);
-      });
+    this.authService.register(this.model).subscribe(
+      () => {
+        this.alertify.success('Registration OK!, please login');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Cancelled!');
+    this.alertify.warning('Register Cancelled!');
   }
-  }
+}
